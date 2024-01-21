4807b5f00f796ab69d4d6f82ebc7434bb7f90ee0
swhite25 <sydney.white@uvm.edu>
2024-01-21 15:48:47 -0500

    Replacing hard coded values

--- generate.py
+++ generate.py
@@ -1,5 +1,14 @@
 import pyrosim.pyrosim as pyrosim
 
+length = 1
+width = 2
+height = 3
+
+x = 0
+y = 0
+# note: change the z variable to half the height to prevent the simulator from growing the link
+z = 1.5
+
 pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
 pyrosim.End()
