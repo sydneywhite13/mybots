5ad38201acef91fb2e6b67f7cc56bfc815abae76
swhite25 <sydney.white@uvm.edu>
2024-02-02 14:19:35 -0500

    simplifying world

--- generate.py
+++ generate.py
@@ -9,10 +9,6 @@ y = 0
 # note: change the z variable to half the height to prevent the simulator from growing the link
 z = 0.5
 
-pyrosim.Start_SDF("boxes.sdf")
-pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
-x2 = 1
-y2 = 0
-z2 = 1.5
-pyrosim.Send_Cube(name="Box2", pos=[x2, y2, z2] , size=[length, width, height])
+pyrosim.Start_SDF("world.sdf")
+pyrosim.Send_Cube(name="Box", pos=[x, y, z ], size=[length, width, height])
 pyrosim.End()
