a67d755c93918a2938d3cd0b916656d0d4c974a7
swhite25 <sydney.white@uvm.edu>
2024-02-02 15:02:23 -0500

    Working on torso and legs

--- generate.py
+++ generate.py
@@ -17,28 +17,13 @@ def Create_World():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     # x,y,z = 0, 0, 0.5 length, width, height = 1,1,1
-    pyrosim.Send_Cube(name="Link0", pos=[x, y, z], size=[length, width, height])
+    pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 2], size=[length, width, height])
     # only the root link has absolute position, everything else is relative to its upstream joint
-    pyrosim.Send_Joint(name="Link0_Link1", parent="Link0", child="Link1", type="revolute", position=[0, 0, 1])
-    pyrosim.Send_Cube(name="Link1", pos=[0, 0, 0.5], size=[length, width, height])
-    pyrosim.Send_Joint(name="Link1_Link2", parent="Link1", child="Link2", type="revolute", position=[0, 0, 1])
-    pyrosim.Send_Cube(name="Link2", pos=[0, 0, 0.5], size=[length, width, height])
 
-    # adding the hook
-    pyrosim.Send_Joint(name="Link2_Link3", parent="Link2", child="Link3", type="revolute", position=[0, 0.5, 0.5])
-    pyrosim.Send_Cube(name="Link3", pos=[0, 0.5, 0], size=[length, width, height])
-
-    # fourth block
-    pyrosim.Send_Joint(name="Link3_Link4", parent="Link3", child="Link4", type="revolute", position=[0, 1, 0])
-    pyrosim.Send_Cube(name="Link4", pos=[0, 0.5, 0], size=[length, width, height])
-
-    # fifth
-    pyrosim.Send_Joint(name="Link4_Link5", parent="Link4", child="Link5", type="revolute", position=[0, 0.5, -0.5])
-    pyrosim.Send_Cube(name="Link5", pos=[0, 0, -0.5], size=[length, width, height])
-
-    # sixth
-    pyrosim.Send_Joint(name="Link5_Link6", parent="Link5", child="Link6", type="revolute", position=[0, 0, -1])
-    pyrosim.Send_Cube(name="Link6", pos=[0, 0, -0.5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[-0.5, 0, -0.5])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[0.5, 0, -0.5])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5], size=[length, width, height])
 
     pyrosim.End()
     # note: relative coordinates are used so that you can make multiple robots only needing to change the root!
