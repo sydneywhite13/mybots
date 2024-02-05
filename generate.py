ff96f3054a7b45921bd1ece62928a8b4c04e00fa
swhite25 <sydney.white@uvm.edu>
2024-02-05 16:04:44 -0500

    last robot with legs

--- generate.py
+++ generate.py
@@ -17,12 +17,12 @@ def Create_World():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     # x,y,z = 0, 0, 0.5 length, width, height = 1,1,1
-    pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 2], size=[length, width, height])
+    pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5], size=[length, width, height])
     # only the root link has absolute position, everything else is relative to its upstream joint
 
-    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[-0.5, 0, -0.5])
+    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[1, 0, 1])
     pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[length, width, height])
-    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[0.5, 0, -0.5])
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[2, 0, 1])
     pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5], size=[length, width, height])
 
     pyrosim.End()
