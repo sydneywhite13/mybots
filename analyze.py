27fbd78a6224e68359c1b2eae0a1a3ddf7de64d4
swhite25 <sydney.white@uvm.edu>
2024-02-11 13:41:43 -0500

    tidying visualization

--- analyze.py
+++ analyze.py
@@ -1,7 +1,10 @@
 import numpy
 import matplotlib.pyplot
 
-backLegSensorValues = numpy.load('data/sensor_output.npy')
-print(backLegSensorValues)
-matplotlib.pyplot.plot(backLegSensorValues)
+backLegSensorValues = numpy.load('data/back_sensor_output.npy')
+frontLegSensorValues = numpy.load('data/front_sensor_output.npy')
+
+matplotlib.pyplot.plot(backLegSensorValues, label='back leg', linewidth=3)
+matplotlib.pyplot.plot(frontLegSensorValues, label='front leg', linewidth=3)
+matplotlib.pyplot.legend()
 matplotlib.pyplot.show()
