53946357792532d96cf5b51a6a5256814cbdeb06
swhite25 <sydney.white@uvm.edu>
2024-02-11 13:41:57 -0500

    adding front sensor

--- simulate.py
+++ simulate.py
@@ -17,13 +17,16 @@ robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 # note: the length of the array must match the for loop index
-backLegSensorValues = numpy.zeros(1000)
-for i in range(1000):
+backLegSensorValues = numpy.zeros(100)
+frontLegSensorValues = numpy.zeros(100)
+for i in range(100):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     time.sleep(1/60)
     #print(i)
-print(backLegSensorValues)
-numpy.save('data/sensor_output.npy', backLegSensorValues)
+numpy.save('data/back_sensor_output.npy', backLegSensorValues)
+numpy.save('data/front_sensor_output.npy', frontLegSensorValues)
+
 p.disconnect()
 
